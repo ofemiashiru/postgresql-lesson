81a59ccf2e078d37b68c5029577426de04569ed1
ofemiashiru <>
2023-06-13 07:10:34 +0000

    SQL Alchemy - Expression

---
+++ sql-expression.py
@@ -0,0 +1,71 @@
+# import modules from sqlalchemy
+from sqlalchemy import (
+    create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
+)
+
+# executing the instructions from our localhost  "chinook" db
+# we use create_engine() to connect and store it in a variable called db
+# the 3 slashes indicate that the database is hosted locally within our
+# workspace environment
+db = create_engine('postgresql:///chinook')
+
+# data about our tables and the data about the data within those tables
+meta = MetaData(db)
+
+
+# create variable for each of our tables that match the schema or
+# database model
+artist_table = Table(
+    "Artist", meta,
+    Column("ArtistId", Integer, primary_key=True),
+    Column("Name", String)
+)
+
+album_table = Table(
+    "Album", meta,
+    Column("AlbumId", Integer, primary_key=True),
+    Column("Title", String),
+    Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
+)
+
+track_table = Table(
+    "Track", meta,
+    Column("TrackId", Integer, primary_key=True),
+    Column("Name", String),
+    Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
+    # when defining columns that are foreign keys that we do not need
+    # set the primary_key property to false
+    Column("MediaTypeId", Integer, primary_key=False),
+    Column("GenreId", Integer, primary_key=False),
+    Column("Composer", String),
+    Column("Milliseconds", Integer),
+    Column("Bytes", Integer),
+    # set UnitPrice to float as it uses decimal values for price
+    Column("UnitPrice", Float)
+
+)
+
+# make a connection to the database using the python with statement (opens and
+# closes the connection for us) and store it in a variable called connection
+
+with db.connect() as connection:
+
+    # query 1 - selecting all from artist table
+    # select_query = artist_table.select()
+
+    # query 2 - select specific columns using the with_only_columns() method
+    # and using .c to identify the specific column we want to return
+    # select_query = artist_table.select().with_only_columns([
+    #     artist_table.c.Name
+    # ])
+
+    # query 3 select values using the WHERE clause
+    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+
+    # query 4 similar to query 3 however we are looking at an id
+    select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    results = connection.execute(select_query)
+
+    for result in results:
+        print(result)
--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -17,16 +17,39 @@ cursor = connection.cursor()
 # Use queries to select items from the databases tables
 # Queries should be in the .execute() method
 # queries should be in single quotes and table names should be in double quotes
-cursor.execute('SELECT * FROM "Artist"')
+# QUERY 1 - All values from a table
+# cursor.execute('SELECT * FROM "Artist"')
+
+# QUERY 2 - Select specific column from a table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# QUERY 3 - Specific value - combination of single and double quotes do not work
+# We need to use a Python string placeholder, and then define the desired
+# string within a list.
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# QUERY 4 - is similar to the above except we us an integer in the list
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# QUERY 5 - getting Albums with the same ArtistId as above
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# QUERY 6 - getting all tracks where the composer is Queen
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# QUERY 7 - find some values on my own - if we change the value to one that is
+# not present in the table an exception will be raised stating that that
+# TypeError:'NoneType' object is not iterable
+cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Aerosmith"])
 
 # Before we query the database we need to set up a way for the data to be
 # retrieved or fetched from the cursor
 
 # for multiple fetch results
-results = cursor.fetchall()
+# results = cursor.fetchall()
 
 # for singular fetch results
-# results = cursor.fetchone()
+results = cursor.fetchone()
 
 # Next, once our results have been fetched, we need to end the connection
 # to the database, so the connection isn't always persistent.
