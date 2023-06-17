452e905097608235dfd04df56dc58c1d71bb35a3
ofemiashiru <>
2023-06-17 14:27:21 +0000

    Adding queries

--- sql-orm.py
+++ sql-orm.py
@@ -10,10 +10,10 @@ from sqlalchemy.orm import sessionmaker
 # connect to our specific database location -This tells the application that
 # we're using the Postgres server, on a local host since there are 3 slashes,
 # in order to connect to our Chinook database.
-db = create_engine("postgres:///chinook")
+db = create_engine("postgresql:///chinook")
 
 # Next, we need a variable called 'base', which will be set to the
-# declarative_base() class. This new 'base' class will essentially grab the
+# declarative_base class. This new 'base' class will essentially grab the
 # metadata that is produced by our database table schema.
 base = declarative_base()
 
@@ -23,17 +23,17 @@ base = declarative_base()
 # Classes defined in python must be written in PascalCase
 
 
-class Artitst(base):
-    __tablename__ = "Artitst"
+class Artist(base):
+    __tablename__ = "Artist"
     ArtistId = Column(Integer, primary_key=True)
-    Artist = Column(String)
+    Name = Column(String)
 
 
 class Album(base):
     __tablename__ = "Album"
     AlbumId = Column(Integer, primary_key=True)
     Title = Column(String)
-    ArtistId = Column(Integer, ForeignKey("Artitst.ArtistId"))
+    ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
 
 
 class Track(base):
@@ -61,3 +61,21 @@ session = Session()
 # we need to then create the database subclass and generate the meta data
 base.metadata.create_all(db)
 
+# create a variable and then use the the .query() method from the session class
+# we can then iterate over the results found and print them out
+
+# Query 1 - select all [The sep="" allows us to place separators between items]
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 2 - select a specifici column
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.Name)
+
+# Query 3 - select artist where the name is "Queen" - we use the filter_by()
+# method to reference the column and the .first() to make sure it only returns
+# the first it comes accross just in case there is more than one result
+artist = session.query(Artist).filter_by(Name="Queen").first()
+print(artist.ArtistId, artist.Name, sep=" | ")
